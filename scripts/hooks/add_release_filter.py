25d3b850da51f990e17d5beb6cc7a54f17218288
Carsten Vuellings <soundfabrick@gmail.com>
2026-03-16 18:10:39 +0100

    - add hook for adding genre filter to release lists (inactive)

--- scripts/hooks/add_release_filter.py
+++ scripts/hooks/add_release_filter.py
@@ -1,8 +1,9 @@
 import logging
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 from mkdocs.config import Config
 from mkdocs.structure.pages import Page
+from markdown.extensions.toc import slugify
 
 from utils.genres import GENRE_TAG_PREFIX, GENRE_TAG_PATTERN, normalize_genre_names
 
@@ -17,8 +18,11 @@ def on_post_page(output: str, page: Page, config: Config) -> str:
 
     soup = BeautifulSoup(output, "html.parser")
 
+    heading = soup.find(name="h1")
+    if not heading:
+        return output
+
     genre_bucket: dict[str, list[str]] = {}
-    genres: list[tuple[str, str]] = []
 
     for p in soup.find_all(name="p"):
         if not GENRE_TAG_PATTERN.match(p.get_text()):
@@ -34,13 +38,20 @@ def on_post_page(output: str, page: Page, config: Config) -> str:
 
         for genre in genre_names_normalized:
             if genre:
-                genres.append((genre, release.get_text().rstrip(" *")))
+                if genre not in genre_bucket:
+                    genre_bucket[genre] = []
+                genre_bucket[genre].append(release.get_text().rstrip(" *"))
 
-    for g, r in genres:
-        if g not in genre_bucket:
-            genre_bucket[g] = []
-        genre_bucket[g].append(r)
+    # print(f"DEBUG: path={src}, genres={genre_bucket.keys()}")
 
-    print(f"DEBUG: path={src}, genres={genre_bucket.keys()}")
+    filter_wrapper_div: Tag = soup.new_tag(name="div", id="genre-filter-wrapper")
+    filter_select_tag: Tag = soup.new_tag(name="select", attrs={"id": "genre-filter-select", "aria-label": "Filter by", "multiple": ""})
+    for genre in genre_bucket.keys():
+        filter_option_tag: Tag = soup.new_tag(name="option", attrs={"value": slugify(genre, "-")})
+        filter_option_tag.string = genre
+        filter_select_tag.append(filter_option_tag)
+
+    filter_wrapper_div.append(filter_select_tag)
+    heading.insert_after(filter_wrapper_div)
 
     return str(soup)
